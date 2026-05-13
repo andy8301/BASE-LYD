7ff009c934539602192e50ba4560d3afa23765fb
andy8301 <38365678+andy8301@users.noreply.github.com>
2026-05-12 22:08:35 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename"/BASE-LYD/">
         <AppLayout>
           <Routes>
             <Route path="/" element={<Dashboard />} />
